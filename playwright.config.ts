343384014c5a93504baeced55bdfe527645ae24c
22-2 <17890m@protonmail.ch>
2026-04-06 13:49:39 +0900

    chore: throw error on Linux to prevent running Playwright tests on unsupported platform

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,8 @@
 import { defineConfig, devices } from "@playwright/test";
+if (process.platform === "linux") {
+    throw new Error("Playwright tests are not supported on Linux due to a known issue with electron.launch on Ubuntu GitHub Actions. Please run the tests on macOS or Windows instead.");
+    // [[BUG] electron.launch: Process failed to launch on Ubuntu github action · Issue #11932 · microsoft/playwright](https://github.com/microsoft/playwright/issues/11932)
+}
 
 export default defineConfig({
     testDir: "./tests",
